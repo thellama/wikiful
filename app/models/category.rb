a52fd3cd071d5f62ffd51cd08647729841d76077
thellama <mmccandless11@gmail.com>
2014-04-11 18:23:44 +0000

    updated seeds.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ActiveRecord::Base
   has_many :article_categories
   has_many :articles, through: :article_categories
 
-  validates :name, presence: true
+  
   validates :name, uniqueness: true
 end
